283c083100e2eac466e9c73ea3e4070ace89ee42
chrishenderson07 <chris.hend012@gmail.com>
2023-09-15 06:03:27 -0300

    Input and Select Created

---
+++ src/components/Input.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Input = styled.input`
+	width: 100%;
+	height: 5.2rem;
+	background-color: #fff;
+	box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+	border-radius: 4px;
+	border: 2px solid #fff;
+	outline: none;
+
+	padding: 0 1.6rem;
+	font-size: 1.6rem;
+
+	transition: border-color 0.2s ease-in;
+	&:focus {
+		border-color: ${({ theme }) => theme.colors.primary.main};
+	}
+`
--- src/pages/NewContact/index.jsx
+++ src/pages/NewContact/index.jsx
@@ -1,10 +1,19 @@
 import { PageHeader } from '../../components/PageHeader'
+import { Input } from '../../components/Input'
+import { Select } from '../../components/Select'
 import { Container } from './styles'
 
 export function NewContact() {
 	return (
 		<Container>
 			<PageHeader title="Novo Contato" />
+			<Input />
+			<Select>
+				<option value="123">Instagram</option>
+				<option value="123">Instagram</option>
+				<option value="123">Instagram</option>
+				<option value="123">Instagram</option>
+			</Select>
 		</Container>
 	)
 }
